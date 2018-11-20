72520c8872682a7167c0574715e47ab76346aac1
Alexander Ela <ela.alexander@gmail.com>
2018-11-19 18:41:18 -0800

    Complete further challenges 1 and 3

---
+++ express-intro/public/index.js
@@ -0,0 +1,7 @@
+const sunsetBtn = document.querySelector('#sunset-btn')
+
+const showSunsets = (e) => {
+	console.log('Sunsets almost ready to log!')
+}
+
+sunsetBtn.addEventListener('click', showSunsets)
