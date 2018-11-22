15f0261c7fcf8f171f313c69c34e7a1f19ea3a26
Alexander Ela <ela.alexander@gmail.com>
2018-11-21 16:42:21 -0800

    Add current Pre-work folder architecture

---
+++ promises-intro/exercise1.js
@@ -0,0 +1,23 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function testNum(num) {
+	return new Promise(function(resolve, reject) {
+		const req = new XMLHttpRequest();
+		req.onload = function() {
+			if (num > 10) {
+				resolve(req.response);
+			} else {
+				reject(Error(req.statusText));
+			}
+		};
+
+		req.onerror = function() {
+			reject(Error("Network Error"));
+		};
+
+		req.send();
+	})
+}
+
+
+testNum(15)
